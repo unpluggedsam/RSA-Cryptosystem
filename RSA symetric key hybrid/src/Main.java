19ca440edfbd0ca717c090ad8d998bf653cd5df8
Sam <unpluggedsam990@gmail.com>
2023-01-16 22:40:11 -0800

    Update Main.java

--- RSA symetric key hybrid/src/Main.java
+++ RSA symetric key hybrid/src/Main.java
@@ -15,23 +15,4 @@ public class Main {
 
 
     }
-
-    private static String convertBinaryToString(int[] binaryArray) {
-        String binary = Arrays.stream(binaryArray)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining());
-
-
-        StringBuilder sb = new StringBuilder(); // Some place to store the chars
-
-        Arrays.stream( // Create a Stream
-                binary.split("(?<=\\G.{8})") // Splits the input string into 8-char-sections (Since a char has 8 bits = 1 byte)
-        ).forEach(s -> // Go through each 8-char-section...
-                sb.append((char) Integer.parseInt(s, 2)) // ...and turn it into an int and then to a char
-        );
-
-        return sb.toString();
-    }
-
-
 }
